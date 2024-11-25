ac2cabd6abebe47b5d2e6911f00937ac1926a36a
sharon504 <sharonpshajan@gmail.com>
2024-11-26 05:10:05 +0530

    Removed unwanted code from lazygit

--- modules/home-manager/nvim/config/plugins/lazygit.nix
+++ modules/home-manager/nvim/config/plugins/lazygit.nix
@@ -3,15 +3,6 @@
   plugins.lazygit = {
     enable = true;
 
-    # Commands that will lazy-load the plugin
-    cmd = [
-      "LazyGit"
-      "LazyGitConfig"
-      "LazyGitCurrentFile"
-      "LazyGitFilter"
-      "LazyGitFilterCurrentFile"
-    ];
-
     # Add dependency
     extraPlugins = [ pkgs.vimPlugins.plenary-nvim ];
   };
