6ef6bcd85ff7e496096755c8cc6263ceea545e6d
sharon504 <sharonpshajan@gmail.com>
2024-11-26 04:33:15 +0530

    Removing unwanted code in clangd.nix

--- modules/home-manager/nvim/config/plugins/lsp/clangd.nix
+++ modules/home-manager/nvim/config/plugins/lsp/clangd.nix
@@ -8,28 +8,5 @@
       inline = false;
     };
 
-    # Use ast configuration directly
-    ast = {
-      # Role icons (requires codicons)
-      roleIcons = {
-        type = "";
-        declaration = "";
-        expression = "";
-        specifier = "";
-        statement = "";
-        "template argument" = "";
-      };
-
-      # Kind icons
-      kindIcons = {
-        Compound = "";
-        Recovery = "";
-        TranslationUnit = "";
-        PackExpansion = "";
-        TemplateTypeParm = "";
-        TemplateTemplateParm = "";
-        TemplateParamObject = "";
-      };
-    };
   };
 }
