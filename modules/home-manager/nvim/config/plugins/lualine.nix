d4e33ad6ced01b820976cf994a0c860db2ef8674
sharon504 <sharonpshajan@gmail.com>
2024-11-26 04:01:21 +0530

    Fixing errors in lualine.nix

--- modules/home-manager/nvim/config/plugins/lualine.nix
+++ modules/home-manager/nvim/config/plugins/lualine.nix
@@ -1,66 +1,126 @@
 { pkgs, ... }:
 {
-  plugins = {
-    lualine = {
-      enable = true;
+  plugins.lualine = {
+    enable = true;
 
-      # Add dependency
-      extraPlugins = [ pkgs.vimPlugins.nvim-web-devicons ];
-
-      # Theme configuration
-      theme = {
-        normal = {
-          a = { bg = "#65D1FF"; fg = "#112638"; gui = "bold"; };
-          b = { bg = "#112638"; fg = "#c3ccdc"; };
-          c = { bg = "#112638"; fg = "#c3ccdc"; };
+    # Theme configuration
+    theme = {
+      normal = {
+        a = {
+          bg = "#65D1FF";
+          fg = "#112638";
+          gui = "bold";
         };
-        insert = {
-          a = { bg = "#3EFFDC"; fg = "#112638"; gui = "bold"; };
-          b = { bg = "#112638"; fg = "#c3ccdc"; };
-          c = { bg = "#112638"; fg = "#c3ccdc"; };
+        b = {
+          bg = "#112638";
+          fg = "#c3ccdc";
         };
-        visual = {
-          a = { bg = "#FF61EF"; fg = "#112638"; gui = "bold"; };
-          b = { bg = "#112638"; fg = "#c3ccdc"; };
-          c = { bg = "#112638"; fg = "#c3ccdc"; };
-        };
-        command = {
-          a = { bg = "#FFDA7B"; fg = "#112638"; gui = "bold"; };
-          b = { bg = "#112638"; fg = "#c3ccdc"; };
-          c = { bg = "#112638"; fg = "#c3ccdc"; };
-        };
-        replace = {
-          a = { bg = "#FF4A4A"; fg = "#112638"; gui = "bold"; };
-          b = { bg = "#112638"; fg = "#c3ccdc"; };
-          c = { bg = "#112638"; fg = "#c3ccdc"; };
-        };
-        inactive = {
-          a = { bg = "#2c3043"; fg = "#semilightgray"; gui = "bold"; };
-          b = { bg = "#2c3043"; fg = "#semilightgray"; };
-          c = { bg = "#2c3043"; fg = "#semilightgray"; };
+        c = {
+          bg = "#112638";
+          fg = "#c3ccdc";
         };
       };
 
-      # Sections configuration
-      sections = {
-        lualine_x =
-          {
-            # Lazy status updates
-            {
-            __raw = ''
-              {
-                require('lazy.status').updates,
-                cond = require('lazy.status').has_updates,
-                color = { fg = "#ff9e64" },
-              }
-            '';
-          }
-            # Standard components
-            { name = "encoding"; }
-            { name = "fileformat"; }
-            { name = "filetype"; }
-          };
+      insert = {
+        a = {
+          bg = "#3EFFDC";
+          fg = "#112638";
+          gui = "bold";
+        };
+        b = {
+          bg = "#112638";
+          fg = "#c3ccdc";
+        };
+        c = {
+          bg = "#112638";
+          fg = "#c3ccdc";
+        };
+      };
+
+      visual = {
+        a = {
+          bg = "#FF61EF";
+          fg = "#112638";
+          gui = "bold";
+        };
+        b = {
+          bg = "#112638";
+          fg = "#c3ccdc";
+        };
+        c = {
+          bg = "#112638";
+          fg = "#c3ccdc";
+        };
+      };
+
+      command = {
+        a = {
+          bg = "#FFDA7B";
+          fg = "#112638";
+          gui = "bold";
+        };
+        b = {
+          bg = "#112638";
+          fg = "#c3ccdc";
+        };
+        c = {
+          bg = "#112638";
+          fg = "#c3ccdc";
+        };
+      };
+
+      replace = {
+        a = {
+          bg = "#FF4A4A";
+          fg = "#112638";
+          gui = "bold";
+        };
+        b = {
+          bg = "#112638";
+          fg = "#c3ccdc";
+        };
+        c = {
+          bg = "#112638";
+          fg = "#c3ccdc";
+        };
+      };
+
+      inactive = {
+        a = {
+          bg = "#2c3043";
+          fg = "#c3ccdc";
+          gui = "bold";
+        };
+        b = {
+          bg = "#2c3043";
+          fg = "#c3ccdc";
+        };
+        c = {
+          bg = "#2c3043";
+          fg = "#c3ccdc";
+        };
       };
     };
+
+    # Sections configuration
+    sections = {
+      lualine_x = [
+        {
+          name = "lazy_status.updates";
+          # In Nixvim, you might need to configure this differently
+          # Depends on how Nixvim handles lazy status updates
+          extraConfig = {
+            cond = "require('lazy.status').has_updates";
+            color = { fg = "#ff9e64"; };
+          };
+        }
+        "encoding"
+        "fileformat"
+        "filetype"
+      ];
+    };
   };
+
+  # Optional: Ensure nvim-web-devicons is installed
+  plugins.nvim-web-devicons.enable = true;
 }
