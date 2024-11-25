9b417ea6489f8017ac349e87f4ba82c23377d90f
sharon504 <sharonpshajan@gmail.com>
2024-11-26 04:42:45 +0530

    Fixing error in comments.nix

--- modules/home-manager/nvim/config/plugins/comments.nix
+++ modules/home-manager/nvim/config/plugins/comments.nix
@@ -5,7 +5,9 @@
       enable = true;
 
       # Pre-hook configuration for JSX/TSX support
-      pre_hook = "require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()";
+      settings = {
+        pre_hook = "require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()";
+      };
     };
 
     # Enable required treesitter context commentstring plugin
