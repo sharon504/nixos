e5b8022a42ddf72441ef6844e388e9f3e8eca163
sharon504 <sharonpshajan@gmail.com>
2024-11-26 04:31:36 +0530

    Fixing error in cland.nix

--- modules/home-manager/nvim/config/plugins/lsp/clangd.nix
+++ modules/home-manager/nvim/config/plugins/lsp/clangd.nix
@@ -2,33 +2,33 @@
   plugins.clangd-extensions = {
     enable = true;
 
-    # Main configuration settings
-    settings = {
-      inlay_hints = {
-        inline = false;
+    # Use inlayHints for inlay hints configuration
+    inlayHints = {
+      enable = true;
+      inline = false;
+    };
+
+    # Use ast configuration directly
+    ast = {
+      # Role icons (requires codicons)
+      roleIcons = {
+        type = "";
+        declaration = "";
+        expression = "";
+        specifier = "";
+        statement = "";
+        "template argument" = "";
       };
 
-      ast = {
-        # Role icons (requires codicons)
-        role_icons = {
-          type = "";
-          declaration = "";
-          expression = "";
-          specifier = "";
-          statement = "";
-          "template argument" = "";
-        };
-
-        # Kind icons
-        kind_icons = {
-          Compound = "";
-          Recovery = "";
-          TranslationUnit = "";
-          PackExpansion = "";
-          TemplateTypeParm = "";
-          TemplateTemplateParm = "";
-          TemplateParamObject = "";
-        };
+      # Kind icons
+      kindIcons = {
+        Compound = "";
+        Recovery = "";
+        TranslationUnit = "";
+        PackExpansion = "";
+        TemplateTypeParm = "";
+        TemplateTemplateParm = "";
+        TemplateParamObject = "";
       };
     };
   };
