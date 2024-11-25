e7cf249207687646c95a40bd8a7d46bc1b4eb1ba
sharon504 <sharonpshajan@gmail.com>
2024-11-26 04:11:15 +0530

    Removing extraConfig for nixvim

--- modules/home-manager/nvim/config/default.nix
+++ modules/home-manager/nvim/config/default.nix
@@ -1,14 +1,4 @@
 {
-  extraConfig = ''
-    vim.cmd("let g:netrw_liststyle=3")
-    vim.api.nvim_create_autocmd("TextYankPost", {
-    	desc = "Highlight when yanking (copying) text",
-    	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    	callback = function()
-    		vim.highlight.on_yank()
-    	end,
-    })
-  '';
   imports = [
     ./options.nix
     ./keymaps.nix
