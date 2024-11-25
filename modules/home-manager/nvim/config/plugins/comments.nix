c5474a9b6710e93c3a317ccedd18b67fd54809dc
sharon504 <sharonpshajan@gmail.com>
2024-11-26 04:48:59 +0530

    Fixed error in comments.nix

--- modules/home-manager/nvim/config/plugins/comments.nix
+++ modules/home-manager/nvim/config/plugins/comments.nix
@@ -5,7 +5,7 @@
       enable = true;
 
       # Pre-hook configuration for JSX/TSX support
-      extraOptions = {
+      settings = {
         pre_hook = "require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()";
       };
     };
