ae060a6aed7061cb47f2caad09d08571686f6e55
sharon504 <sharonpshajan@gmail.com>
2024-11-26 16:43:49 +0530

    Modifying to fix error in treesitter

--- modules/home-manager/nvim/config/plugins/treesitter.nix
+++ modules/home-manager/nvim/config/plugins/treesitter.nix
@@ -2,55 +2,56 @@
   plugins = {
     treesitter = {
       enable = true;
+      settings = {
+        # Syntax highlighting
+        highlight.enable = true;
 
-      # Syntax highlighting
-      highlight.enable = true;
+        # Indentation
+        indent.enable = true;
 
-      # Indentation
-      indent.enable = true;
-
-      # Incremental selection with corrected keymaps
-      incrementalSelection = {
-        enable = true;
-        keymaps = {
-          initSelection = {
-            action = "<C-space>";
-          };
-          nodeIncremental = {
-            action = "<C-space>";
-          };
-          scopeIncremental = {
-            action = false;
-          };
-          nodeDecremental = {
-            action = "<bs>";
+        # Incremental selection with corrected keymaps
+        incrementalSelection = {
+          enable = true;
+          keymaps = {
+            initSelection = {
+              action = "<C-space>";
+            };
+            nodeIncremental = {
+              action = "<C-space>";
+            };
+            scopeIncremental = {
+              action = false;
+            };
+            nodeDecremental = {
+              action = "<bs>";
+            };
           };
         };
-      };
 
-      # Ensure these language parsers are installed
-      ensureInstalled = [
-        "json"
-        "javascript"
-        "typescript"
-        "tsx"
-        "yaml"
-        "html"
-        "css"
-        "prisma"
-        "markdown"
-        "markdown_inline"
-        "svelte"
-        "graphql"
-        "bash"
-        "lua"
-        "vim"
-        "dockerfile"
-        "gitignore"
-        "query"
-        "vimdoc"
-        "c"
-      ];
+        # Ensure these language parsers are installed
+        ensureInstalled = [
+          "json"
+          "javascript"
+          "typescript"
+          "tsx"
+          "yaml"
+          "html"
+          "css"
+          "prisma"
+          "markdown"
+          "markdown_inline"
+          "svelte"
+          "graphql"
+          "bash"
+          "lua"
+          "vim"
+          "dockerfile"
+          "gitignore"
+          "query"
+          "vimdoc"
+          "c"
+        ];
+      };
     };
 
     # Enable nvim-ts-autotag plugin
