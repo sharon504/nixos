e48b8d55dd6b88f092c6cb8a05fa58d1d3b1c7e0
sharon504 <sharonpshajan@gmail.com>
2024-11-26 20:42:19 +0530

    Fixing typo

--- modules/home-manager/nvim/config/plugins/formatting.nix
+++ modules/home-manager/nvim/config/plugins/formatting.nix
@@ -2,22 +2,25 @@
   plugins.conform-nvim = {
     enable = true;
 
+
     # Formatter configurations by file type
-    formatters_by_ft = {
-      javascript = [ "prettier" ];
-      typescript = [ "prettier" ];
-      javascriptreact = [ "prettier" ];
-      typescriptreact = [ "prettier" ];
-      svelte = [ "prettier" ];
-      css = [ "prettier" ];
-      html = [ "prettier" ];
-      json = [ "prettier" ];
-      yaml = [ "prettier" ];
-      markdown = [ "prettier" ];
-      graphql = [ "prettier" ];
-      liquid = [ "prettier" ];
-      lua = [ "stylua" ];
-      python = [ "isort" "black" ];
+    settings = {
+      formatters_by_ft = {
+        javascript = [ "prettier" ];
+        typescript = [ "prettier" ];
+        javascriptreact = [ "prettier" ];
+        typescriptreact = [ "prettier" ];
+        svelte = [ "prettier" ];
+        css = [ "prettier" ];
+        html = [ "prettier" ];
+        json = [ "prettier" ];
+        yaml = [ "prettier" ];
+        markdown = [ "prettier" ];
+        graphql = [ "prettier" ];
+        liquid = [ "prettier" ];
+        lua = [ "stylua" ];
+        python = [ "isort" "black" ];
+      };
     };
 
   };
