abd9a0fb1f8d5061e740693a0b8b09dc1e81279f
sharon504 <sharonpshajan@gmail.com>
2024-11-26 21:03:26 +0530

    Correcting lspconfig

--- modules/home-manager/nvim/config/plugins/lsp/lspconfig.nix
+++ modules/home-manager/nvim/config/plugins/lsp/lspconfig.nix
@@ -54,135 +54,67 @@
       };
 
       # Configure keymaps
-      onAttach = {
-        function = true;
-        mappings = [
-          {
-            mode = "n";
-            key = "gR";
-            action = "<cmd>Telescope lsp_references<CR>";
-            options = {
-              desc = "Show LSP references";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "gD";
-            action = "vim.lsp.buf.declaration";
-            lua = true;
-            options = {
-              desc = "Go to declaration";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "gd";
-            action = "<cmd>Telescope lsp_definitions<CR>";
-            options = {
-              desc = "Show LSP definitions";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "gi";
-            action = "<cmd>Telescope lsp_implementations<CR>";
-            options = {
-              desc = "Show LSP implementations";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "gt";
-            action = "<cmd>Telescope lsp_type_definitions<CR>";
-            options = {
-              desc = "Show LSP type definitions";
-              silent = true;
-            };
-          }
-          {
-            mode = [ "n" "v" ];
-            key = "<leader>ca";
-            action = "vim.lsp.buf.code_action";
-            lua = true;
-            options = {
-              desc = "See available code actions";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "<leader>rn";
-            action = "vim.lsp.buf.rename";
-            lua = true;
-            options = {
-              desc = "Smart rename";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "<leader>D";
-            action = "<cmd>Telescope diagnostics bufnr=0<CR>";
-            options = {
-              desc = "Show buffer diagnostics";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "<leader>d";
-            action = "vim.diagnostic.open_float";
-            lua = true;
-            options = {
-              desc = "Show line diagnostics";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "[d";
-            action = "vim.diagnostic.goto_prev";
-            lua = true;
-            options = {
-              desc = "Go to previous diagnostic";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "]d";
-            action = "vim.diagnostic.goto_next";
-            lua = true;
-            options = {
-              desc = "Go to next diagnostic";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "K";
-            action = "vim.lsp.buf.hover";
-            lua = true;
-            options = {
-              desc = "Show documentation for what is under cursor";
-              silent = true;
-            };
-          }
-          {
-            mode = "n";
-            key = "<leader>rs";
-            action = ":LspRestart<CR>";
-            options = {
-              desc = "Restart LSP";
-              silent = true;
-            };
-          }
-        ];
-      };
+      onAttach = ''
+            function()
+        		-- import lspconfig plugin
+        		local lspconfig = require("lspconfig")
+
+        		-- import mason_lspconfig plugin
+        		local mason_lspconfig = require("mason-lspconfig")
+
+        		-- import cmp-nvim-lsp plugin
+        		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+        		local keymap = vim.keymap -- for conciseness
+
+        		vim.api.nvim_create_autocmd("LspAttach", {
+        			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        			callback = function(ev)
+        				-- Buffer local mappings.
+        				-- See `:help vim.lsp.*` for documentation on any of the below functions
+        				local opts = { buffer = ev.buf, silent = true }
+
+        				-- set keybinds
+        				opts.desc = "Show LSP references"
+        				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+        				opts.desc = "Go to declaration"
+        				keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+        				opts.desc = "Show LSP definitions"
+        				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+        				opts.desc = "Show LSP implementations"
+        				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+        				opts.desc = "Show LSP type definitions"
+        				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+        				opts.desc = "See available code actions"
+        				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+        				opts.desc = "Smart rename"
+        				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+        				opts.desc = "Show buffer diagnostics"
+        				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+        				opts.desc = "Show line diagnostics"
+        				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+        				opts.desc = "Go to previous diagnostic"
+        				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+        				opts.desc = "Go to next diagnostic"
+        				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+        				opts.desc = "Show documentation for what is under cursor"
+        				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+        				opts.desc = "Restart LSP"
+        				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+        			end,
+      '';
     };
 
     # Enable dependencies
