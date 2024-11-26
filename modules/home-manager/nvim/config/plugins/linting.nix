2c848636ac34ce94af4feb7837fa60ad9206f34e
sharon504 <sharonpshajan@gmail.com>
2024-11-26 16:51:24 +0530

    Fixing typo in linting.nix

--- modules/home-manager/nvim/config/plugins/linting.nix
+++ modules/home-manager/nvim/config/plugins/linting.nix
@@ -15,22 +15,20 @@
       svelte = [ "eslint_d" ];
       python = [ "pylint" ];
     };
-    config = {
-      extraConfigLua = ''
-        local lint = require("lint")
+    extraConfigLua = ''
+      local lint = require("lint")
       
-        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-          group = lint_augroup,
-          callback = function()
-            lint.try_lint()
-          end,
-        })
-
-        vim.keymap.set("n", "<leader>l", function()
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
           lint.try_lint()
-        end, { desc = "Trigger linting for current file" })
-      '';
-    };
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>l", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    '';
   };
 }
