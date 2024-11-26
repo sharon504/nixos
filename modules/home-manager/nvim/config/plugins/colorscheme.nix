22469f30c33e733da2804b4b10137790a11dacd4
sharon504 <sharonpshajan@gmail.com>
2024-11-26 22:26:26 +0530

    Fixing colorscheme config file

--- modules/home-manager/nvim/config/plugins/colorscheme.nix
+++ modules/home-manager/nvim/config/plugins/colorscheme.nix
@@ -1,3 +1,37 @@
 {
-  colorscheme = "tokyonight-night";
+  colorschemes.tokyonight = {
+    settings = {
+      style = "night";
+      light_style = "day";
+      transparent = true;
+      terminal_colors = true;
+
+      styles = {
+        comments = { italic = true; };
+        keywords = { italic = true; };
+        functions = { };
+        variables = { };
+
+        sidebars = "dark";
+        floats = "dark";
+      };
+
+      sidebars = [ "qf" "help" ];
+      floats = [ "lsp" "notify" "diff" "telescope" ];
+
+      dim_inactive = true;
+      lualine_bold = false;
+      lsp_trouble = false;
+
+      modules = [
+        "which-key"
+        "indent-blankline"
+        "bufferline"
+        "cmp"
+        "nvim-cmp"
+        "gitsigns"
+        "telescope"
+      ];
+    };
+  };
 }
