dd25da6d8da29f2c27583492a8aab427163c3f7c
sharon504 <sharonpshajan@gmail.com>
2024-11-26 20:56:57 +0530

    Updating wrong configuration in telescope.nix

--- modules/home-manager/nvim/config/plugins/telescope.nix
+++ modules/home-manager/nvim/config/plugins/telescope.nix
@@ -1,91 +1,88 @@
 {
-  # Telescope plugin configuration
-  plugins.telescope = {
-    enable = true;
+  plugins = {
+    # Telescope plugin configuration
+    fzf.enable = true;
+    todo-comments.enable = true;
+    fzf-native.enable = true;
+    web-devicons.enable = true;
+    telescope = {
+      enable = true;
 
-    # Use attribute sets for extensions
-    enabledExtensions = {
-      plenary.enable = true;
-      fzf.enable = true;
-      todo-comments.enable = true;
-      fzf-native.enable = true;
-      web-devicons.enable = true;
-    };
-
-    # Basic telescope configuration
-    settings = {
-      defaults = {
-        pathDisplay = [ "smart" ];
-        mappings = {
-          i = {
-            "<C-k>" = "move_selection_previous";
-            "<C-j>" = "move_selection_next";
-            "<C-q>" = {
-              action = ''
-                function()
-                  local actions = require("telescope.actions")
-                  local trouble = require("trouble")
-                  actions.send_selected_to_qflist()
-                  trouble.toggle("quickfix")
-                end
-              '';
-              lua = true;
-            };
-            "<C-t>" = {
-              action = ''
-                function()
-                  require("trouble.sources.telescope").open()
-                end
-              '';
-              lua = true;
+      # Basic telescope configuration
+      settings = {
+        defaults = {
+          pathDisplay = [ "smart" ];
+          mappings = {
+            i = {
+              "<C-k>" = "move_selection_previous";
+              "<C-j>" = "move_selection_next";
+              "<C-q>" = {
+                action = ''
+                  function()
+                    local actions = require("telescope.actions")
+                    local trouble = require("trouble")
+                    actions.send_selected_to_qflist()
+                    trouble.toggle("quickfix")
+                  end
+                '';
+                lua = true;
+              };
+              "<C-t>" = {
+                action = ''
+                  function()
+                    require("trouble.sources.telescope").open()
+                  end
+                '';
+                lua = true;
+              };
             };
           };
         };
       };
     };
-  };
 
-  # Keymaps configuration
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>ff";
-      action = "<cmd>Telescope find_files<cr>";
-      options = {
-        desc = "Fuzzy find files in cwd";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>fr";
-      action = "<cmd>Telescope oldfiles<cr>";
-      options = {
-        desc = "Fuzzy find recent files";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>fs";
-      action = "<cmd>Telescope live_grep<cr>";
-      options = {
-        desc = "Find string in cwd";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>fc";
-      action = "<cmd>Telescope grep_string<cr>";
-      options = {
-        desc = "Find string under cursor in cwd";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>ft";
-      action = "<cmd>TodoTelescope<cr>";
-      options = {
-        desc = "Find todos";
-      };
-    }
-  ];
+    # Keymaps configuration
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>ff";
+        action = "<cmd>Telescope find_files<cr>";
+        options = {
+          desc = "Fuzzy find files in cwd";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fr";
+        action = "<cmd>Telescope oldfiles<cr>";
+        options = {
+          desc = "Fuzzy find recent files";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fs";
+        action = "<cmd>Telescope live_grep<cr>";
+        options = {
+          desc = "Find string in cwd";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fc";
+        action = "<cmd>Telescope grep_string<cr>";
+        options = {
+          desc = "Find string under cursor in cwd";
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>ft";
+        action = "<cmd>TodoTelescope<cr>";
+        options = {
+          desc = "Find todos";
+        };
+      }
+    ];
+  };
 }
