2bedcdf332b13135292a52c5cf6100fdcfd184ea
sharon504 <sharonpshajan@gmail.com>
2024-11-26 16:49:36 +0530

    Fixing linting.nix extraConfig

--- modules/home-manager/nvim/config/plugins/linting.nix
+++ modules/home-manager/nvim/config/plugins/linting.nix
@@ -15,21 +15,22 @@
       svelte = [ "eslint_d" ];
       python = [ "pylint" ];
     };
-
-    extraConfigLua = ''
-      local lint = require("lint")
+    config = {
+      extraConfigLua = ''
+        local lint = require("lint")
       
-      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
-        end,
-      })
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+          group = lint_augroup,
+          callback = function()
+            lint.try_lint()
+          end,
+        })
 
-      vim.keymap.set("n", "<leader>l", function()
-        lint.try_lint()
-      end, { desc = "Trigger linting for current file" })
-    '';
+        vim.keymap.set("n", "<leader>l", function()
+          lint.try_lint()
+        end, { desc = "Trigger linting for current file" })
+      '';
+    };
   };
 }
