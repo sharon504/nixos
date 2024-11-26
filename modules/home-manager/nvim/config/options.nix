226f5456090997fa5f8707006b656831aea11109
sharon504 <sharonpshajan@gmail.com>
2024-11-26 21:55:17 +0530

    changing booleans from string type to bool

--- modules/home-manager/nvim/config/options.nix
+++ modules/home-manager/nvim/config/options.nix
@@ -1,23 +1,23 @@
 {
   config.opts = {
     # Basic settings
-    relativenumber = "true";
-    number = "true";
+    relativenumber = true;
+    number = true;
 
     # Tabs & indentation
     tabstop = 2; # 2 spaces for tabs (prettier default)
     shiftwidth = 2; # 2 spaces for indent width
-    expandtab = "true"; # expand tab to spaces
-    autoindent = "true"; # copy indent from current line when starting new one
-    wrap = "false";
+    expandtab = true; # expand tab to spaces
+    autoindent = true; # copy indent from current line when starting new one
+    wrap = false;
 
     # Search settings
-    ignorecase = "true"; # ignore case when searching
-    smartcase = "true"; # if you include mixed case in your search, assumes you want case-sensitive
-    cursorline = "true";
+    ignorecase = true; # ignore case when searching
+    smartcase = true; # if you include mixed case in your search, assumes you want case-sensitive
+    cursorline = true;
 
     # Colors and UI
-    termguicolors = "true"; # turn on termguicolors for colorscheme support
+    termguicolors = true; # turn on termguicolors for colorscheme support
     background = "dark"; # colorschemes that can be light or dark will be made dark
     signcolumn = "yes"; # show sign column so that text doesn't shift
 
@@ -28,17 +28,17 @@
     clipboard = "unnamedplus"; # use system clipboard as default register
 
     # Split windows
-    splitright = "true"; # split vertical window to the right
-    splitbelow = "true"; # split horizontal window to the bottom
+    splitright = true; # split vertical window to the right
+    splitbelow = true; # split horizontal window to the bottom
 
     # Disable swapfile
-    swapfile = "false";
+    swapfile = false;
 
     # Highlight on yank
     highlight = {
       # Create an autocommand for highlighting yanked text
       onYank = {
-        enable = "true";
+        enable = true;
         timeout = 200;
       };
     };
