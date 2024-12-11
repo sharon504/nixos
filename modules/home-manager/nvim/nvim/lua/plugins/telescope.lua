e80e24d9641685505eeabd4e090451999c996462
sharon504 <sharonpshajan@gmail.com>
2024-12-11 15:20:31 +0530

    Editing telescope config

--- modules/home-manager/nvim/nvim/lua/plugins/telescope.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		-- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-telescope/telescope-fzf-native.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
 	},
