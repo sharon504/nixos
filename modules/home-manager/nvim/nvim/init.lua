cae1782f62494cb0f6d3ce9b5b10155632bb4877
sharon504 <sharonpshajan@gmail.com>
2024-12-11 14:36:13 +0530

    Commenting some code that lazy initialises

--- modules/home-manager/nvim/nvim/init.lua
+++ modules/home-manager/nvim/nvim/init.lua
@@ -12,12 +12,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
-})
+-- require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
+-- 	checker = {
+-- 		enabled = true,
+-- 		notify = false,
+-- 	},
+-- 	change_detection = {
+-- 		notify = false,
+-- 	},
+-- })
