1942d1bd81572733c8d53df07680490f57fe2409
sharon504 <sharonpshajan@gmail.com>
2025-04-15 01:56:22 +0530

    Fixed: hypridle listeners

--- modules/home-manager/hypridle/default.nix
+++ modules/home-manager/hypridle/default.nix
@@ -6,43 +6,43 @@
     enable = true;
     settings = {
       general = {
-        lock_cmd = "${pkgs.procps}/bin/pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
-        before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
-        after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
       };
 
       listeners = [
         # Brightness dimming
         {
           timeout = 150;
-          on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";
-          on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
         }
 
         # Keyboard backlight
         {
           timeout = 150;
-          on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -sd rgb:kbd_backlight set 0";
-          on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -rd rgb:kbd_backlight";
+          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+          on-resume = "brightnessctl -rd rgb:kbd_backlight";
         }
 
         # Lock session
         {
           timeout = 300;
-          on-timeout = "${pkgs.systemd}/bin/loginctl lock-session";
+          on-timeout = "loginctl lock-session";
         }
 
         # DPMS (Display Power Management Signaling)
         {
           timeout = 330;
-          on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
-          on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
         }
 
         # System suspend
         {
           timeout = 1800;
-          on-timeout = "${pkgs.systemd}/bin/systemctl suspend";
+          on-timeout = "systemctl suspend";
         }
       ];
     };
