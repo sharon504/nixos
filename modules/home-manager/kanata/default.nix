c089697ed37511f85ea39e573e8c501f1717e9e5
sharon504 <sharonpshajan@gmail.com>
2025-04-14 18:03:22 +0530

    Added kanata config

---
+++ modules/home-manager/kanata/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, config, lib, ... }:
+{
+  home.file = {
+    ".config/kanata" = {
+      source = ./kanata;
+      recursive = true;
+    };
+  };
+  services.kanata = {
+    enable = true;
+    extraConfig = builtins.readFile ./kanata/config.kbd
+  };
+}
