332b301e63b7b7ba4106ba808cebb941e21593cb
sharon504 <sharonpshajan@gmail.com>
2025-04-17 06:14:19 +0530

    Fixed some scripts

--- modules/home-manager/hyprland/hypr/scripts/ch_wallpaper.sh
+++ modules/home-manager/hyprland/hypr/scripts/ch_wallpaper.sh
@@ -1,33 +1,10 @@
-#!/bin/sh
-wallpapersDir="/home/alpha/.config/hypr/.wallpapers"
-trans="--transition-fps 60 --transition-type outer --transition-step 90"
-# Get a list of all image files in the wallpapers directory
-wallpapers=("$wallpapersDir"/*)
+#!/usr/bin/env bash
 
-# Start an infinite loop
-while true; do
-    # Check if the wallpapers array is empty
-    if [ ${#wallpapers[@]} -eq 0 ]; then
-        # If the array is empty, refill it with the image files
-        wallpapers=("$wallpapersDir"/*)
-    fi
+WALLPAPER_DIR="$HOME/.config/hypr/.wallpapers"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
-    # Select a random wallpaper from the array
-    wallpaperIndex=$(( RANDOM % ${#wallpapers[@]} ))
-    selectedWallpaper="${wallpapers[$wallpaperIndex]}"
+# Get a random wallpaper that is not the current one
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
-    # Remove the selected wallpaper from the array
-    swww img "$selectedWallpaper" $trans
-    unset "wallpapers[$wallpaperIndex]"
-
-    # Delay for 2 hours before selecting the next wallpaper
-    # wal -i $(cat ~/.cache/swww/eDP-1)
-    # bash ~/.config/waybar/launch.sh
-    # rm -rf ~/.cache/current_wallpaper.jpg
-    # cp  $(cat ~/.cache/swww/eDP-1) ~/.cache/current_wallpaper.jpg
-    # magick ~/.cache/current_wallpaper.jpg ~/.cache/current_wallpaper.png
-    # rm -rf ~/.cache/current_wallpaper.jpg
-    # echo $(cat ~/.cache/wal/colors-waybar.css) > ~/.cache/wal/colors-wlogout.css
-    sleep 2h
-done
-
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
