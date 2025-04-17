faf7ba7acf114fb6ee9859363b9443981f2d8a16
sharon504 <sharonpshajan@gmail.com>
2025-04-17 20:34:53 +0530

    Fixing congif

--- modules/home-manager/hypridle/default.nix
+++ modules/home-manager/hypridle/default.nix
@@ -1,11 +1,49 @@
-{ config, lib, pkgs, ... }:
-
 {
-  home.file = {
-    ".config/hypr/hypridle.conf" = ./hyprildle.conf;
-  };
-  # Hypridle configuration
   services.hypridle = {
     enable = true;
+
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+
+      listeners = [
+        # Dim screen after 2.5 minutes
+        {
+          timeout = 150;
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+        }
+
+        # Turn off keyboard backlight after 2.5 minutes
+        {
+          timeout = 150;
+          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+          on-resume = "brightnessctl -rd rgb:kbd_backlight";
+        }
+
+        # Lock screen after 5 minutes
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+
+        # Turn off display after 5.5 minutes
+        {
+          timeout = 330;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+
+        # Suspend after 30 minutes
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
   };
 }
+
