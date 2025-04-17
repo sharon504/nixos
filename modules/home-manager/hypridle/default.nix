21bdce38af8800aa4abd7f4372ee44af20e3b612
sharon504 <sharonpshajan@gmail.com>
2025-04-17 20:20:53 +0530

    Fixing hypridle config error

--- modules/home-manager/hypridle/default.nix
+++ modules/home-manager/hypridle/default.nix
@@ -15,34 +15,34 @@
         # Brightness dimming
         {
           timeout = 150;
-          on-timeout = "brightnessctl -s set 10";
+          "on-timeout" = "brightnessctl -s set 10";
           on-resume = "brightnessctl -r";
         }
 
         # Keyboard backlight
         {
           timeout = 150;
-          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
-          on-resume = "brightnessctl -rd rgb:kbd_backlight";
+          "on-timeout" = "brightnessctl -sd rgb:kbd_backlight set 0";
+          "on-resume" = "brightnessctl -rd rgb:kbd_backlight";
         }
 
         # Lock session
         {
           timeout = 300;
-          on-timeout = "loginctl lock-session";
+          "on-timeout" = "loginctl lock-session";
         }
 
         # DPMS (Display Power Management Signaling)
         {
           timeout = 330;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
+          "on-timeout" = "hyprctl dispatch dpms off";
+          "on-resume" = "hyprctl dispatch dpms on";
         }
 
         # System suspend
         {
           timeout = 1800;
-          on-timeout = "systemctl suspend";
+          "on-timeout" = "systemctl suspend";
         }
       ];
     };
