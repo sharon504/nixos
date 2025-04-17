a931e5ea4e08df5817130aa56393f08cd3ab5b76
sharon504 <sharonpshajan@gmail.com>
2025-04-17 07:35:22 +0530

    Fixed waybar config

--- modules/home-manager/waybar/default.nix
+++ modules/home-manager/waybar/default.nix
@@ -1,11 +1,7 @@
 {
-  home.file = {
-  ".config/waybar/" = ./waybar; 
-  };
-
+  home.file.".config/waybar" = { source = ./waybar; };
   programs.waybar = {
     enable = true;
     style = builtins.readFile ./waybar/style.css;
-    };
   };
 }
