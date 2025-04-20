93620af69f735a3644b12bdaea2a3e513908e1d0
sharon504 <sharonpshajan@gmail.com>
2025-04-20 19:18:57 +0530

    Adding copilot

--- modules/home-manager/nvim/nvim/lua/plugins/copilot.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/copilot.lua
@@ -2,19 +2,19 @@
 -- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/copilot.lua
 
 return {
-	-- "zbirenbaum/copilot.lua",
-	-- opts = {
-	-- 	suggestion = {
-	-- 		enabled = false,
-	-- 		auto_refresh = true,
-	-- 	},
-	-- 	panel = {
-	-- 		enabled = true,
-	-- 		auto_refresh = true,
-	-- 	},
-	-- 	filetypes = {
-	-- 		markdown = true,
-	-- 		help = true,
-	-- 	},
-	-- },
+	"zbirenbaum/copilot.lua",
+	opts = {
+		suggestion = {
+			enabled = false,
+			auto_refresh = true,
+		},
+		panel = {
+			enabled = true,
+			auto_refresh = true,
+		},
+		filetypes = {
+			markdown = true,
+			help = true,
+		},
+	},
 }
