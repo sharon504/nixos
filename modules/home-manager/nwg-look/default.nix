de2933d78abc8e5214f3624fda3175d4ce192898
sharon504 <sharonpshajan@gmail.com>
2025-05-04 08:48:43 +0530

    Fixed symlinks

--- modules/home-manager/nwg-look/default.nix
+++ modules/home-manager/nwg-look/default.nix
@@ -1,20 +1,17 @@
 { pkgs, config, lib, ... }:
 {
   home.file = {
-    ".config/gtk-4.0" = {
-      source = ./gtk-4.0;
-      recursive = true;
-    };
-    ".config/gtk-3.0" = {
-      source = ./gtk-3.0;
-      recursive = true;
-    };
+    # ".config/gtk-4.0" = {
+    #   source = ./gtk-4.0;
+    #   recursive = true;
+    # };
+    # ".config/gtk-3.0" = {
+    #   source = ./gtk-3.0;
+    #   recursive = true;
+    # };
     ".config/nwg-look" = {
       source = ./nwg-look;
       recursive = true;
     };
-  };
-  programs.nwg-look= {
-    enable = true;
   };
 }
