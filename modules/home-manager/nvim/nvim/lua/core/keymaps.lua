b1a6582091a129e4ebcc1f0b439700b6ac39899a
sharon504 <sharonpshajan@gmail.com>
2025-05-06 08:44:06 +0530

    Adding and configuring some plugins

--- modules/home-manager/nvim/nvim/lua/core/keymaps.lua
+++ modules/home-manager/nvim/nvim/lua/core/keymaps.lua
@@ -88,3 +88,22 @@ end, { desc = "Source the nvimrc" })
 vim.keymap.set("n", "<Esc>", "<Esc>:noh<CR>", { desc = "remap <Esc> key to remove search highlights" })
 vim.keymap.set("i", "<Esc>", "<Esc>:noh<CR>", { desc = "remap <Esc> key to remove search highlights" })
 vim.keymap.set("n", "<leader><leader>sc", "<cmd>source %<CR>", { desc = "source config files" })
+
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set(
+  "n", 
+  "<leader>a", 
+  function()
+    vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
+    -- or vim.lsp.buf.codeAction() if you don't want grouping.
+  end,
+  { silent = true, buffer = bufnr }
+)
+vim.keymap.set(
+  "n", 
+  "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+  function()
+    vim.cmd.RustLsp({'hover', 'actions'})
+  end,
+  { silent = true, buffer = bufnr }
+)
--- modules/home-manager/nvim/nvim/lua/plugins/rustaceanvim.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/rustaceanvim.lua
@@ -1,9 +1,5 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^5", -- Recommended
-	lazy = false, -- This plugin is already lazy
-	ft = "rust", -- Only load on Rust files
-	init = function()
-		vim.g.rustfmt_autosave = 1
-	end,
+  'mrcjkb/rustaceanvim',
+  version = '^6', -- Recommended
+  lazy = false, -- This plugin is already lazy
 }
